6d8885243870de4ba8ede26ce93e24264b1410ff
Pedro Ferreira <85911000+pedro8777@users.noreply.github.com>
2021-11-03 14:42:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const linksSocialMedia = {
     for(let li of socialLinks.children) {
      const social = li.getAttribute('class')
       
-      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`   
+      li.children[0].href = `https://${social}.com/${linksSocialMedia}`   
 
     } 
 }
